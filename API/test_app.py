dd4715f4e273aa0b09b0223bb68d46e5641b3284
Ugochukwu-On <onwuu1@gmail.com>
2021-08-02 09:19:40 -0700

    Made a function to hold the API, updated integrated testing, tested the get url an response which passed, currently debugging the post method.

--- API/test_app.py
+++ API/test_app.py
@@ -1,21 +1,34 @@
 import unittest
-import API
-
+from API.app import app
+from app import Notes_list
 
 class TestApp(unittest.TestCase):
     def setUp(self):
-
-        self.client = API.create_app().test_client()
+        self.client = app.test_client()
 
 
     def test_GET(self):
-       rv = self.Notes.get('/Notes')
-       assert isinstance(rv.json, object)
-       assert'List of Notes' in rv.json
+        tester = app.test_client(self)
+        response = tester.get("/Notes")
+        status_code = response.status_code
+        self.assertEqual(status_code,200)
+
+    def test_POST(self):
+        tester = app.test_client(self)
+        response = tester.post('/Notes/<int:id>',
+        Notes_list (
+            [
+             {
+              "id": 2,
+              "body": "Yesterday was a good day",
+              }
+            ]
+
+         ))
+
+        status_code = response.status_code
+        self.assertEqual(status_code,200)
 
 
 if __name__ == "__main__":
     unittest.main()
-
-
-
