c143a1e9e47665c3b5cb9c298e3d5efe2b2b44f1
Ugochukwu-On <onwuu1@gmail.com>
2021-08-17 06:44:39 -0700

    Integrated test for the COUNTRIES_API

--- API/test_app.py
+++ API/test_app.py
@@ -15,12 +15,12 @@ class TestApp(unittest.TestCase):
         self.assertEqual(status_code,200)
 
     def test_POST_new_notes(self):
-        Notes_list = dict(id=4, body="Yo")
-
+        #Notes_list = dict(id=4, body="Yo")
         tester = app.test_client(self)
-        request = tester.post('/Notes/', Notes_list)
-        status_code = request.status_code
-        self.assertEqual(status_code,201)
+        requests = tester.get('/Notes')
+        requests.post({'id': 4, 'body': "yo"})
+        status_code = requests.status_code
+        self.assertEqual(status_code,200)
 
 
 if __name__ == "__main__":
