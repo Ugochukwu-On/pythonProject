0915c5fcef25ff36e06c29cb885bb8e15fde3de2
Ugochukwu-On <onwuu1@gmail.com>
2021-09-06 01:33:38 -0700

    added a new endpoint

--- COUNTRIES_API/app.py
+++ COUNTRIES_API/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask,jsonify
 import json
 app = Flask(__name__)
 json_file = open('C:\\Users\\DELL\\PycharmProjects\\pythonProject\\COUNTRIES_API\\countries.json', 'r',
@@ -9,8 +9,9 @@ The App would be read from a json file
 The API would be able to get infromation from the json file with endpoints
 Example:
     GET/all- Get all data in the json file
-    GET/country - to get only the countries in the json file
+    GET/countries - to get only the countries in the json file
     GET/country/states - to get all the states of all countries
+    GET/countries/Country- to get all the states i a country
 '''
 
 
@@ -27,7 +28,7 @@ def all_countries():
 
 
 
-@app.route('/country', methods=['GET'])
+@app.route('/countries', methods=['GET'])
 def only_country():
     data = json_file.read()
     object = json.loads(data)
@@ -41,7 +42,7 @@ def only_country():
     return jsonify(location), 200
 
 @app.route('/country/states', methods=['GET'])
-def only_states():
+def states():
     data = json_file.read()
     object = json.loads(data)
     location = []
@@ -53,7 +54,18 @@ def only_states():
 
     return jsonify(location), 200
 
-
+@app.route('/countries/<CountryName>', methods=['GET'])
+def only_states(CountryName):
+    json_file = open('C:\\Users\\DELL\\PycharmProjects\\pythonProject\\COUNTRIES_API\\countries.json', 'r',
+                     encoding='utf-8')
+    data = json_file.read()
+    object = json.loads(data)
+    for places in object["countries"]:
+        y = places.get("country")
+        if y == CountryName:
+            x = places.get("states")
+            print(x)
+    return jsonify(CountryName), 200
 
 
 if __name__ == "__main__":
